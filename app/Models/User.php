f5bd51d8ede7a4e3ff1f3d23660945155354186d
Salma GTU <salmajaddar7@gmail.com>
2026-04-06 18:10:14 +0200

    feat(User): implémentation complète du modèle utilisateur avec gestion des rôles, cast et méthodes utilitaires

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable{
        'score_confiance' => 'integer',
     ];
 
-  
+ 
     public function estStandard(): bool{
         return $this->role === 'standard';
     }
