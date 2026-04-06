fa7a71abaea41d8d0885118fe5a317d0d0129dcd
Salma GTU <salmajaddar7@gmail.com>
2026-04-06 18:37:34 +0200

    Création de AuthController avec la méthode afficherLogin pour afficher la page de connexion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller{
     
-    public function showLogin(){
+    public function afficherlogin(){
         return view('auth.login');
     }
     
