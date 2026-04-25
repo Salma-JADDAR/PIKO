c01b79bbebdef35a8f9adc78eee768a0b36c1227
Salma GTU <salmajaddar7@gmail.com>
2026-04-25 13:15:36 +0200

    Ajout de la colonne raison_rejet dans la table annonces afin de stocker la raison du rejet d’une annonce lors du processus de validation

--- database/migrations/2026_04_20_090458_add_raison_rejet_to_annonces_table.php
+++ database/migrations/2026_04_20_090458_add_raison_rejet_to_annonces_table.php
@@ -4,17 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up()
-    {
+return new class extends Migration{
+    public function up(){
         Schema::table('annonces', function (Blueprint $table) {
             $table->text('raison_rejet')->nullable()->after('etat');
         });
     }
 
-    public function down(): void
-    {
+    public function down(): void{
         Schema::table('annonces', function (Blueprint $table) {
             $table->dropColumn('raison_rejet');
         });
